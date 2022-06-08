5222f80fa4be5c939367fcb494f0be445f7a1695
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2022-06-08 17:07:53 +0400

    custom dialog for the selecting brush size done

--- app/src/main/java/com/elchinasgarov/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/elchinasgarov/kidsdrawingapp/MainActivity.kt
@@ -1,18 +1,50 @@
 package com.elchinasgarov.kidsdrawingapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import com.elchinasgarov.DrawingView
 import com.elchinasgarov.kidsdrawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var drawingView: DrawingView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        drawingView = findViewById(R.id.drawing_view)
+        val brush: ImageButton = findViewById(R.id.brush_btn)
+        brush.setOnClickListener {
+            showBrushChooserDialog()
+        }
 
-        binding.drawingView.setSizeForBrush(20.toFloat())
 
+    }
 
+    private fun showBrushChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size :")
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.small_brush)
+        smallBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+
+        }
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.medium_brush)
+        mediumBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+
+        }
+        val largeBtn: ImageButton = brushDialog.findViewById(R.id.large_brush)
+        largeBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+
+        }
+
+        brushDialog.show()
     }
 }
