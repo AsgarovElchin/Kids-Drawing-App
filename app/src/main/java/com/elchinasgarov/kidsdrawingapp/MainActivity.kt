bbf22209620991a7465855012c42430fa1f66716
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2022-06-09 22:27:03 +0400

    adding permission request done

--- app/src/main/java/com/elchinasgarov/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/elchinasgarov/kidsdrawingapp/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.elchinasgarov.kidsdrawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.elchinasgarov.DrawingView
@@ -14,7 +19,30 @@ import com.elchinasgarov.kidsdrawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
-    private var mImageButtonCurrentPaint : ImageButton? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult((ActivityResultContracts.RequestMultiplePermissions())) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+                if (isGranted) {
+                    Toast.makeText(
+                        this,
+                        "Permission granted now you can read the storage files",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    if (permissionName == android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                        Toast.makeText(
+                            this,
+                            "Oops you just denied the permission",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                }
+            }
+
+        }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,11 +54,16 @@ class MainActivity : AppCompatActivity() {
         val linearLayout = findViewById<LinearLayout>(R.id.linear_layout_colors)
         mImageButtonCurrentPaint = linearLayout[1] as ImageButton
         mImageButtonCurrentPaint!!.setImageDrawable(
-            ContextCompat.getDrawable(this,R.drawable.selected_colors)
+            ContextCompat.getDrawable(this, R.drawable.selected_colors)
         )
         val brush: ImageButton = findViewById(R.id.brush_btn)
         brush.setOnClickListener {
             showBrushChooserDialog()
+        }
+
+        val importButton: ImageButton = findViewById(R.id.import_btn)
+        importButton.setOnClickListener {
+            requestStoragePermission()
         }
 
 
@@ -62,19 +95,49 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
     }
 
-    fun paintClicked(view:View){
-       if(view!=mImageButtonCurrentPaint){
-           val imageButton = view as ImageButton
-           val colorTag = imageButton.tag.toString()
-           drawingView?.setColor(colorTag)
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationableDialog(
+                "Kids Drawing App",
+                "Kids Drawing App" + "needs to access your External Storage"
+            )
 
-           imageButton.setImageDrawable(
-               ContextCompat.getDrawable(this,R.drawable.selected_colors)
-           )
-           mImageButtonCurrentPaint?.setImageDrawable(
-               ContextCompat.getDrawable(this,R.drawable.pallet_normal)
-           )
-           mImageButtonCurrentPaint = view
-       }
+        } else {
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            )
+        }
+    }
+
+
+    private fun showRationableDialog(title: String, messag: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(messag)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
+    fun paintClicked(view: View) {
+        if (view != mImageButtonCurrentPaint) {
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
+
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.selected_colors)
+            )
+            mImageButtonCurrentPaint?.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+            mImageButtonCurrentPaint = view
+        }
     }
 }
